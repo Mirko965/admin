801f02ea2980df3e92d4d3eb2f935a59d9a87acc
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-31 14:56:25 +0200

    uploading files

---
+++ index.php
@@ -0,0 +1,9 @@
+<form action="" method="post" enctype="multipart/form-data" id="uploadImage">
+<p>
+<label for="image">Upload image:</label>
+<input type="file" name="image" id="image">
+</p>
+<p>
+<input type="submit" name="upload" id="upload" value="Upload">
+</p>
+</form>
