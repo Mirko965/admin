77fe09bad73f0d475115e6a21dfc668c7b1c4274
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-31 18:44:18 +0200

    Creating a basic file upload script

--- uploads/file_uploads.php
+++ uploads/file_uploads.php
@@ -1,3 +1,14 @@
+<?php
+// set the maximum upload size in bytes
+$max = 51200;
+if (isset($_POST['upload'])) {
+// define the path to the upload folder
+$destination = 'C:/upload_test/';
+// move the file to the upload folder and rename it
+move_uploaded_file($_FILES['image']['tmp_name'],
+$destination . $_FILES['image']['name']);
+}
+?>
 <!doctype html>
 <html>
     <head>
@@ -12,19 +23,14 @@
          <form action="" method="post" enctype="multipart/form-data" id="uploadImage">
            <p>
              <label for="image">Upload image:</label>
+               <input type="hidden" name="MAX_FILE_SIZE" value="<?= $max; ?>">
              <input type="file" name="image" id="image">
            </p>
            <p>
               <input type="submit" name="upload" id="upload" value="Upload">
            </p>
          </form>
-        <pre>
-        <?php
-        if (isset($_POST['upload'])) {
-        print_r($_FILES);
-        }
-        ?>
-        </pre>
+
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="js/main.js"></script>
     </body>
