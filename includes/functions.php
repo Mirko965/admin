abd11cd289c13873d61d08487ee68c3361539fa0
Mirko965 <mirkojelic.jelic@gmail.com>
2016-06-01 21:14:33 +0200

    prikaz tabele

---
+++ includes/functions.php
@@ -0,0 +1,12 @@
+<?php
+function find_all_admins(){
+    global $dbconn;
+
+    $query_admin  = "SELECT * " ;
+    $query_admin .= "FROM admin ";
+    $result_admin = mysqli_query($dbconn, $query_admin);
+    if(!$result_admin){
+        die("Databases failed");
+    }
+    return($result_admin);
+}
