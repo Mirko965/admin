f59bddafec873da92097076200a3f5db4071da20
Mirko965 <mirkojelic.jelic@gmail.com>
2016-06-02 22:05:57 +0200

    function find_admin_by_id

--- includes/functions.php
+++ includes/functions.php
@@ -15,3 +15,23 @@ function redirect_to($new_location) {
 		header("Location: " . $new_location);
 		exit;
 	}
+
+function find_admin_by_id($admin_id){
+		global $dbconn;
+
+		$safe_admin_id = mysqli_real_escape_string($dbconn, $admin_id);
+
+		$query  = "SELECT * ";
+		$query .= "FROM admin ";
+		$query .= "WHERE id = {$safe_admin_id} ";
+		$query .= "LIMIT 1";
+		$result = mysqli_query($dbconn, $query);
+		 if(!$result){
+        die("Databases failed");
+        }
+		if($admin = mysqli_fetch_assoc($result)) {
+			return $admin;
+		} else {
+			return null;
+		}
+}
