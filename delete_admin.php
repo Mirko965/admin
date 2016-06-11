1c36401d3f012999be8c8e5ea9f5569ff69c3ef9
Mirko965 <mirkojelic.jelic@gmail.com>
2016-06-11 10:57:23 +0200

    ovo ne radi????

--- delete_admin.php
+++ delete_admin.php
@@ -1,4 +1,18 @@
 <?php require_once("./includes/dbconnection.php") ?>
 <?php require_once("./includes/functions.php") ?>
 
+<?php
+$admin = find_admin_by_id($_GET["id"]);
 
+if(!$admin){
+    redirect_to("index.php");
+}
+
+$id = $admin["id"];
+$query = "DELETE FROM admin WHERE id = {$id}";
+$result = mysqli_query($dbconn,$query);
+if($result && mysqli_affected_rows($dbconn)){
+    return $result;
+    redirect_to("index.php");
+}
+?>
